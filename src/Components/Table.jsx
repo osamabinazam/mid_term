1b4db22a9c37474d0b9bbdb88288a3e870eb8856
Osama Bin Azam <osamabinazam@gmail.com>
2023-10-17 11:36:31 +0500

    Add Button To Reset All Filters

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -5,17 +5,58 @@ import { useState } from 'react'
 function Table() {
     // Populating the table with the data from the 
     const [sample, setSample] = useState(sampleData)
-
     const [myForm, setMyForm] = useState({
         fullName: '',
         age:'',
+        city: [],
+        occuptions:[]
 
     })
+
+    const cities = new Set(sample.filter((sample) => {
+        return sample.city
+    }))
+
+    const occuptions = new Set(sample.filter((sample) => {
+        return sample.occuptions
+    }))
+
+    console.log(cities)
+    console.log(occuptions)
+    
+    // 
+    const updateTable = ()=>{
+        const filtered_data = sampleData.filter((sample)=>{
+            
+            // if (sample.name.toLowerCase() === myForm.fullName.toLowerCase() && sample.age === myForm.age){
+            //     // console.log("hfsdufds")
+            //     return sample
+            // }
+            // else
+            //     // console.log(sample.name , sample.age, myForm.fullName, myForm.age )
+            
+            
+        })
+        console.log(filtered_data)
+    }
+
+
 
 
     // Handle Runtime Change in the Fields
     const handleChange = (e) => {
-        const {name, type, value} = e.target
+        
+
+        const {name, type, value, selectedOptions } = e.target
+        if (name === 'city' || name === 'occuptions'){
+
+            console.log(Array.from(selectedOptions, (option) => option.value))
+            setMyForm({
+                ...myForm,
+                [name] : Array.from(selectedOptions, (option) => option.value)
+            })
+        }
+
         setMyForm({
           ...myForm,
             [name]: value
@@ -24,7 +65,8 @@ function Table() {
 
     // Handle Reset Filters
     const handleReset = () => {
-
+        console.log("Reset Button is clicked")
+        setSample(sampleData)
     }
 
 
@@ -40,7 +82,6 @@ function Table() {
 
         console.log(filtered_data)
         setSample(filtered_data)
-
     }
 
 
@@ -74,8 +115,29 @@ function Table() {
             <br />
             <h1>Filter Fields</h1>
             <form >
-                    <input type="text" placeholder='Name' name='fullName' onChange={handleChange}/>   <br />    
+                    <input type="text" placeholder='Name' name='fullName' onChange={handleChange}/><br />    
                     <input type="text" placeholder='Age' name='age' onChange={handleChange}/>
+
+                    {/* Drop Down Menus  */}
+
+                    <div>
+                        <label htmlFor="">City </label>
+
+                        <select name="city" onChange={handleChange}  >
+                        {cities.map((item, index) => (
+                            <option value={item.city.toLowerCase()}>{item.city}</option>
+                        ))}
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Occuptions: </label>
+                        <select name="occuptions" onChange={handleChange} >
+                        {occuptions.map((item, index) => (
+                            <option value={item.occupation.toLowerCase()}>{item.occupation}</option>
+                        ))}
+                        </select>
+                    </div>
             </form>
 
             <br />
